6055fe5c964dbbc501c49baed07603da349253fc
thasipablo <thasipablo@gmail.com>
2022-12-22 11:56:34 +0100

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Box>
+      <Typography variant="h1">Home</Typography>
+    </Box>
+  );
+};
+
+export default Home;
