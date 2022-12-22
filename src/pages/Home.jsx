05902b36c8b00d19bc36b0ec156b8713007d1d0e
thasipablo <thasipablo@gmail.com>
2022-12-22 15:25:48 +0100

    welcome message added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import { Typography } from "@mui/material";
+import { blueGrey } from "@mui/material/colors";
 import { Box } from "@mui/system";
 import React from "react";
 
 const Home = () => {
   return (
-    <Box>
-      <Typography variant="h1">Home</Typography>
+    <Box
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography variant="h4" color={blueGrey[700]} fontWeight={600}>
+        Welcome to Altavic
+      </Typography>
+      <Typography variant="body" color={blueGrey[300]}>
+        Were to find solution
+      </Typography>
     </Box>
   );
 };
