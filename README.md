fbf343b23a66859b6201c22c1e59fcc3905204b2
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-01-28 18:13:43 +0100

    init with tailwind and pnpm

--- README.md
+++
@@ -1,1 +0,0 @@
-# Altavic
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+  return (
+    <main className="flex justify-center items-center min-h-screen">
+      <div className="text-center">
+        <h1 className="text-2xl font-bold text-gray-700">Welcome to Altavic</h1>
+        <p className="text-gray-400">
+          Move beyond the limits of your imagination.
+        </p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
